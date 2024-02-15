bd04e8e72368b50ce978d727741a2a9a59e21f95
Georgy Saukov <georgy.saukov@sap.com>
2024-02-15 15:49:33 +0100

    more samples

--- src/app/preview-app/framework-app/component-component/component-component.component.ts
+++ src/app/preview-app/framework-app/component-component/component-component.component.ts
@@ -7,36 +7,36 @@ import { Component } from '@angular/core';
 })
 export class ComponentComponentComponent {
   code = '@Component({\n' +
-    '  selector: \'app-component-component\',\n' +
-    '  styleUrl: \'./component-component.component.scss\',\n' +
-    '  template: \'<mat-card>\\n\' +\n' +
-    '    \'  <mat-card-content>\\n\' +\n' +
-    '    \'    <h1> Component. </h1>\\n\' +\n' +
-    '    \'    <div class="grid-container">\\n\' +\n' +
-    '    \'      <div class="grid-item item1">\\n\' +\n' +
-    '    \'        <span> Fundamental element of Angular! </span>\\n\' +\n' +
-    '    \'        <ol>\\n\' +\n' +
-    '    \'          <li><strong>Template:</strong></li>\\n\' +\n' +
-    '    \'          <p>Represents the isolated view of the component, defining its DOM structure and bindings.</p>\\n\' +\n' +
-    '    \'          <li><strong>Class:</strong></li>\\n\' +\n' +
-    '    \'          <p>Represents the TypeScript class associated with the component. It facilitates the manipulation of the template, performs calculations, and reacts to lifecycle events through methods known as lifecycle hooks.</p>\\n\' +\n' +
-    '    \'          <li><strong>Styles:</strong></li>\\n\' +\n' +
-    '    \'          <p>Consist of CSS styles that are applied to the components template, providing visual decoration and layout.</p>\\n\' +\n' +
-    '    \'          <li><strong>Test:</strong></li>\\n\' +\n' +
-    '    \'          <p>Refers to the tools and methodologies used to test the behavior and functionality of the component, ensuring its correctness and reliability.</p>\\n\' +\n' +
-    '    \'        </ol>\\n\' +\n' +
-    '    \'        <br/>\\n\' +\n' +
-    '    \'      </div>\\n\' +\n' +
-    '    \'      <div class="grid-item item2">\\n\' +\n' +
-    '    \'        <app-editor [code]="code"></app-editor>\\n\' +\n' +
-    '    \'      </div>\\n\' +\n' +
-    '    \'    </div>\\n\' +\n' +
-    '    \'  </mat-card-content>\\n\' +\n' +
-    '    \'</mat-card>\'\n' +
-    '})' +
+    '  selector: app-component-component,\n' +
+    '  styleUrl: ./component-component.component.scss,\n' +
+    '  template: <mat-card> \n' +
+    '      <mat-card-content> \n' +
+    '        <h1> Component. </h1> \n' +
+    '        <div class="grid-container"> \n' +
+    '          <div class="grid-item item1"> \n' +
+    '            <span> Fundamental element of Angular! </span> \n' +
+    '            <ol> \n' +
+    '              <li><strong>Template:</strong></li> \n' +
+    '              <p>Represents the isolated view of the component, defining its DOM structure and bindings.</p> \n' +
+    '              <li><strong>Class:</strong></li> \n' +
+    '              <p>Represents the TypeScript class associated with the component. It facilitates the manipulation of the template, performs calculations, and reacts to lifecycle events through methods known as lifecycle hooks.</p> \n' +
+    '              <li><strong>Styles:</strong></li> \n' +
+    '              <p>Consist of CSS styles that are applied to the components template, providing visual decoration and layout.</p> \n' +
+    '              <li><strong>Test:</strong></li> \n' +
+    '              <p>Refers to the tools and methodologies used to test the behavior and functionality of the component, ensuring its correctness and reliability.</p> \n' +
+    '            </ol> \n' +
+    '            <br/> \n' +
+    '          </div> \n' +
+    '          <div class="grid-item item2"> \n' +
+    '            <app-editor [code]="code"></app-editor> \n' +
+    '          </div> \n' +
+    '        </div> \n' +
+    '      </mat-card-content> \n' +
+    '    </mat-card>\n' +
+    '}) \n' +
     'export class FrameworkAppComponent {\n' +
-    '\n' +
-    '  constructor() { }\n' +
+    '\n code="code-that-you-see-in-editor" \n ' +
+    'constructor() { }\n' +
     '\n' +
     '  ngOnInit(): void {\n' +
     '\n' +
