9ae3500666f42686847fdc72a83d2910048c0e99
Georgy Saukov <georgy.saukov@sap.com>
2024-02-21 16:57:57 +0100

    Fake testing

--- src/app/preview-app/testing-app/testing-app.component.spec.ts
+++ src/app/preview-app/testing-app/testing-app.component.spec.ts
@@ -1,6 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { TestingAppComponent } from './testing-app.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {TestingAppComponent} from './testing-app.component';
+import {BrowserModule, By} from "@angular/platform-browser";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule } from "@angular/forms";
+import {MatImportsModule} from "../../mat-imports.module";
+import {MonacoEditorModule} from "ngx-monaco-editor-v2";
+import {EditorComponent} from "../editor/editor.component";
 
 describe('TestingAppComponent', () => {
   let component: TestingAppComponent;
@@ -8,16 +13,26 @@ describe('TestingAppComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TestingAppComponent]
+      imports: [BrowserModule,
+        HttpClientModule,
+        FormsModule,
+        MatImportsModule,
+        EditorComponent,
+        MonacoEditorModule.forRoot()
+      ],
+      declarations: [TestingAppComponent],
     })
-    .compileComponents();
-    
+      .compileComponents();
     fixture = TestBed.createComponent(TestingAppComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component and data', () => {
     expect(component).toBeTruthy();
+    const monacoEditorElement = fixture.debugElement.query(By.css('ngx-monaco-editor')).nativeElement as HTMLElement;
+    expect(monacoEditorElement?.textContent?.trim()).toBe('MY CODE');
+    expect(component.editor.model.value).toBe('MY CODE');
   });
+
 });
