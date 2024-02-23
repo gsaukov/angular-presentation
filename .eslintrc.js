5f28ff86a18a4cf47f7586c42d544b31c526a879
Georgy Saukov <georgy.saukov@sap.com>
2024-02-23 14:21:12 +0100

    lint support

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  ignorePatterns: ['projects/**/*'],
+  overrides: [
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: [
+          'tsconfig.app.json',
+          'tsconfig.spec.json'
+        ],
+        createDefaultProgram: true
+      },
+      plugins: [
+        '@typescript-eslint',
+      ],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:prettier/recommended',
+      ],
+      rules: {
+        '@typescript-eslint/semi': 'off',
+        'prettier/prettier': ['error', {
+          'endOfLine': 'auto',
+        }],
+      }
+    },
+  ],
+};
